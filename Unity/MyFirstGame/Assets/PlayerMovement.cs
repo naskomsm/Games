bcad5665a1d9e35f6b5b1dc0ca5fbf8c3affa54a
Atanas Kolev <naskokolev00@gmail.com>
2020-05-10 14:31:30 +0300

    playing with unity and learning it

---
+++ Unity/MyFirstGame/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysFroce = 500f;
+
+    // Use "FixedUpdate" instead of "Update" when using Unity physics
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysFroce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysFroce * Time.deltaTime, 0, 0);
+        }
+    }
+}
